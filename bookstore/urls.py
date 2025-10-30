69d05aed9961efa2eff40c9a47befa25913965ee
htferretti <htferretti@gmail.com>
2025-10-30 15:48:36 -0300

    docker compose

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    re_path(r'^bookstore/(?P<version>(v1|v2))/', include('product.urls')),
-    re_path(r'^bookstore/(?P<version>(v1|v2))/', include('order.urls')),
+    re_path(r"^bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    re_path(r"^bookstore/(?P<version>(v1|v2))/", include("order.urls")),
 ]

0d7547f1fdd5df0441be385804cc15cb4f5a9e49
htferretti <htferretti@gmail.com>
2025-11-03 15:28:55 -0300

    secret key

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -171,3 +171,4 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]").split(" ")
+SECRET_KEY = os.getenv("SECRET_KEY", "dummy-secret-key-for-tests")

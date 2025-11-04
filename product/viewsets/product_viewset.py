68ab4cea434fae1e8679ecb1f89795f81c7397fa
htferretti <htferretti@gmail.com>
2025-11-03 22:37:07 -0300

    fix

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,17 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
-# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.authentication import TokenAuthentication 
-from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.permissions import AllowAny
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
-
 class ProductViewSet(ModelViewSet):
-    # authentication_classes = [SessionAuthentication, BasicAuthentication]
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
-
+    permission_classes = [AllowAny] 
+    
     def get_queryset(self):
         return Product.objects.all().order_by("id")
